69b35ec2df44f3921b86bd0235fe9bfd273840f1
TyGosley <tygosley@gmail.com>
2023-04-04 18:10:42 -0700

    added comments to model index.js

--- Main/models/index.js
+++ Main/models/index.js
@@ -10,4 +10,17 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, Post, Comment };
